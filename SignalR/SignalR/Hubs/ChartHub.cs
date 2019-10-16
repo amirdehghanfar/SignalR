17d4714d7d0c6f4c2d935b9014f125db75fb711f
amir <amirdehghanfar@gamil.com>
2019-10-15 22:43:39 -0700

    first commit

---
+++ SignalR/SignalR/Hubs/ChartHub.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalR.Hubs
+{
+    public class ChartHub : Hub
+    {
+
+        public Task SendMessageToUser(string connectionId, string message)
+        {
+            return Clients.Client(connectionId).SendAsync("RecieveMessage", message);
+        }
+
+        public override Task OnConnectedAsync()
+        {
+            Clients.All.SendAsync("UserConnected", Context.ConnectionId);
+            return base.OnConnectedAsync();
+        }
+        public override Task OnDisconnectedAsync(Exception exception)
+        {
+            Clients.All.SendAsync("UserDisConnected", Context.ConnectionId);
+            return base.OnDisconnectedAsync(exception);
+        }
+
+    }
+}
